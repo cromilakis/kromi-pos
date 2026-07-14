0d7a9c8498f084ecb4f63f96dcaf7cdc6ae89b1d
Cromilakis <ipcromilakis@gmail.com>
2026-07-14 11:15:35 -0400

    feat(tauri): habilitar updater (plugins, capability, config GitHub Releases)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -62,6 +62,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![greet, print_receipt, print_cierre, print_quote, print_credit_note, save_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
